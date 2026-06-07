88eb72efdb757a677909a114fef2332d21706808
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-06-07 18:28:15 +0200

    add app state

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@
 use axum::{Router, routing::get};
 use tower_http::cors::CorsLayer;
 
+mod state;
+use state::AppState;
+    
 async fn home() -> &'static str {
     "Hello, from home page!"
 }
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(home)).layer(CorsLayer::permissive());
+    let db = sqlx::SqlitePool::connect("sqlite:./db.sqlite")
+        .await
+        .unwrap();
+    let app_state = AppState { db };
+    let app = Router::new()
+        .route("/", get(home))
+        .with_state(app_state)
+        .layer(CorsLayer::permissive());
     let address = "0.0.0.0:3000";
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
