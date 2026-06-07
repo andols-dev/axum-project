4dbcd73939b9f3a41f06cb296aa8edc9d8a8c64b
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-06-07 18:09:12 +0200

    install react js. add tower-http

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 // axum
 use axum::{Router, routing::get};
+use tower_http::cors::CorsLayer;
 
 async fn home() -> &'static str {
     "Hello, from home page!"
 }
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(home));
+    let app = Router::new().route("/", get(home)).layer(CorsLayer::permissive());
     let address = "0.0.0.0:3000";
     let listener = tokio::net::TcpListener::bind(address).await.unwrap();
     axum::serve(listener, app).await.unwrap();
