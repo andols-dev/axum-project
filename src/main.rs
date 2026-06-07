3ac0c9bbbe7ad35c1fca9c4657d5c482ee3e633c
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-06-07 17:03:02 +0200

    update tokio, create server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// axum
+use axum::{Router, routing::get};
+
+async fn home() -> &'static str {
+    "Hello, from home page!"
+}
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(home));
+    let address = "0.0.0.0:3000";
+    let listener = tokio::net::TcpListener::bind(address).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
