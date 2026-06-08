a16889002134eb673482fffc0d24124438bd8edf
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-06-08 13:13:00 +0200

    add layout component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
 import "./App.css";
+import Layout from "./components/layout/Layout.jsx";
 import { Link } from "react-router";
 
 function App() {
   return (
     <>
-      <h1 class="text-3xl font-bold ">Axum Frontend project</h1>
-      <Link
-        to="/login"
-        class="cursor-pointer text-blue-500 hover:text-blue-700"
-      >
-        Login
-      </Link>
+      <Layout>
+        <h1 class="text-3xl font-bold ">Axum Frontend project</h1>
+        <Link
+          to="/login"
+          class="cursor-pointer text-blue-500 hover:text-blue-700"
+        >
+          Login
+        </Link>
+      </Layout>
     </>
   );
 }
