d523374f390e21d88efe8f4842fa30545e8b1a28
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-07-25 13:11:34 +0200

    create table

---
+++ migrations/20260725110220_create_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    created_at TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
