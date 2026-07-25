1fcd5bbdd14233503f84219da5e535865eff7594
Andreas Olsson <60063451+andols-dev@users.noreply.github.com>
2026-07-25 09:05:13 +0200

    add login validation

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import { Link } from "react-router";
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  function validateForm(formData) {
+    const errors = {};
+
+    if (!formData.username.trim()) {
+      errors.username = "User name is required";
+    }
+
+    if (!formData.password.trim()) {
+      errors.password = "Password is required";
+    }
+
+    return errors;
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const validationErrors = validateForm(formData);
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
+    setErrors({});
+
+    console.log("Form submitted:", formData);
+  };
   return (
     <div>
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-3xl font-bold mb-4">Login</h1>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+        >
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -16,9 +63,15 @@ const Login = () => {
             <input
               className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
               type="text"
               placeholder="Username"
             />
+            {errors.username && (
+              <p className="text-red-500 text-xs italic">{errors.username}</p>
+            )}
           </div>
           <div className="mb-6">
             <label
@@ -30,14 +83,19 @@ const Login = () => {
             <input
               className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
+              name="password"
+              value={formData.password}
               type="password"
               placeholder="Password"
             />
+            {errors.password && (
+              <p className="text-red-500 text-xs italic">{errors.password}</p>
+            )}
           </div>
           <div className="flex items-center justify-between">
             <button
               className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
+              type="submit"
             >
               Login
             </button>
